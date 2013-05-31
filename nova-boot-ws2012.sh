946a8a95a4e29674a85095f86426eecdfab9515d
alexpilotti <ap@pilotti.it>
2013-06-01 01:28:58 +0300

    Updated WS 12 nova boot

--- nova-boot-ws2012.sh
+++ nova-boot-ws2012.sh
@@ -1,5 +1,5 @@
 NETID1=`quantum net-show net1 | awk '{if (NR == 5) {print $4}}'`
 NETID2=`quantum net-show net2 | awk '{if (NR == 5) {print $4}}'`
 
-nova boot --config-drive false  --flavor 13 --image "Windows Server 2012 Std Eval" --key-name key1 --nic net-id=$NETID1 vm1
+nova boot  --flavor 13 --image "Windows Server 2012 Std Eval" --key-name key1 --nic net-id=$NETID1 vm1
 
