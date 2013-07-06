096ddfc7a0992110fcf27252b67833c4944d050d
Alessandro Pilotti <ap@pilotti.it>
2013-07-06 23:42:23 +0300

    Updates quantum networks removal

--- quantum-delete-all.sh
+++ quantum-delete-all.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
-quantum net-list | awk '{if (NR > 3 && $2 != "") {system("quantum net-delete " $2);}}'
+SUBNETID1=`quantum net-show net1 | awk '{if (NR == 13) {print $4}}'`
+quantum router-interface-delete router1 $SUBNETID1
+quantum net-delete net1
+
+SUBNETID2=`quantum net-show net2 | awk '{if (NR == 13) {print $4}}'`
+quantum router-interface-delete router1 $SUBNETID2
+quantum net-delete net2
+
+quantum router-delete router1
+
+quantum net-delete ext_net
 
