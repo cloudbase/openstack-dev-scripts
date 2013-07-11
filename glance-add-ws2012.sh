122284b1d2afbf580397b018cbaa968e193cb5be
root <root@os-controller.cbs>
2013-07-11 21:19:17 +0100

    Changes glance add ws2012 original image location
    This is suitable for VMWare fusion

--- glance-add-ws2012.sh
+++ glance-add-ws2012.sh
@@ -1,3 +1,3 @@
-VHD=/mnt/hgfs/Downloads/windows_server_2012_standard_eval_hyperv_20130531.vhd.gz
+VHD=/mnt/hgfs/Images/windows_server_2012_standard_eval_hyperv_20130531.vhd.gz
 gunzip -cd $VHD | glance image-create --property hypervisor_type=hyperv --name "Windows Server 2012 Std Eval" --container-format bare --disk-format vhd
 
