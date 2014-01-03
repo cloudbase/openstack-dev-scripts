2d88454928918b0d9581fedda5cf1e1257906780
Alessandro Pilotti <apilotti@cloudbasesolutions.com>
2014-01-03 22:33:59 +0200

    Update glance-add-qemu.sh

--- glance-add-qemu.sh
+++ glance-add-qemu.sh
@@ -1,1 +1,1 @@
-glance image-create --property hypervisor_type=QEMU --name "Ubuntu 12.04 QEMU" --container-format bare --disk-format qcow2 < /mnt/hgfs/Downloads/ubuntu-12.04-server-cloudimg-amd64-disk1.img 
+glance image-create --property hypervisor_type=qemu --name "Ubuntu 12.04 QEMU" --container-format bare --disk-format qcow2 < /mnt/hgfs/Downloads/ubuntu-12.04-server-cloudimg-amd64-disk1.img 
