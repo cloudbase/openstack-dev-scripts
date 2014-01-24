515d3e69d93254282a33559a2af48a679b9e6b50
Alessandro Pilotti <apilotti@cloudbasesolutions.com>
2014-01-24 15:15:26 -0500

    Add Glance image from shared folder

--- glance-add-centos64.sh
+++ glance-add-centos64.sh
@@ -1,3 +1,3 @@
-VHD=/mnt/hgfs/Images/centos64_1.vhd.gz
-gunzip -cd $VHD | glance image-create --property hypervisor_type=hyperv --name "CentOS64" --container-format bare --disk-format vhd
+VHD=/mnt/hgfs/Downloads/CentOS-6.4-x86_64-Minimal-OpenStack.image.vpc.bz2
+bunzip2 -cd $VHD | glance image-create --property hypervisor_type=hyperv --name "CentOS64" --container-format bare --disk-format vhd
 
