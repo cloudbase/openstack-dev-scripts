c7e17d319751f89fed0ce022922956c789c05777
Alessandro Pilotti <apilotti@cloudbasesolutions.com>
2014-02-18 22:04:14 +0200

    Update glance-add-ws2012.sh

--- glance-add-ws2012.sh
+++ glance-add-ws2012.sh
@@ -1,3 +1,3 @@
 VHD=/mnt/hgfs/Images/windows_server_2012_standard_eval_hyperv_20130531.vhd.gz
-gunzip -cd $VHD | glance image-create --property hypervisor_type=hyperv --name "Windows Server 2012 Std Eval" --container-format bare --disk-format vhd
+gunzip -cd $VHD | glance image-create --property hypervisor_type=hyperv --name "Windows Server 2012 Std Eval" --container-format bare --disk-format vhd -min-disk 20 --min-ram 1024
 
