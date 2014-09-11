6dc9768dc0416158e4089da7be355fd059dc6f9d
Alessandro Pilotti <apilotti@cloudbasesolutions.com>
2014-09-11 15:41:03 +0300

    Use default network and flavor for cirros boot

--- nova-boot-cirros.sh
+++ nova-boot-cirros.sh
@@ -1,3 +1,3 @@
-NETID1=`neutron net-show net1 | awk '{if (NR == 5) {print $4}}'`
-nova boot  --flavor m1.micro --image "cirros" --key-name key1 --nic net-id=$NETID1 vm1
+NETID1=`neutron net-show private | awk '{if (NR == 5) {print $4}}'`
+nova boot  --flavor m1.tiny --image "cirros" --key-name key1 --nic net-id=$NETID1 vm1
 
